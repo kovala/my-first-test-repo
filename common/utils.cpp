d91deb19f849fd58f0b440876baf985dafba1011
Cype Core <akoval@cypecore.com>
2024-11-11 10:43:04 +1100

    new algos

--- common/utils.cpp
+++ common/utils.cpp
@@ -7,15 +7,14 @@ namespace arrays {
   std::vector<int> findDuplicates(const std::vector<int>& arr) {
     std::priority_queue<int, std::vector<int>, std::greater<int>> minHeap;
 
-
-    for (int i = 0; i < arr.size(); i++) {
-      int k = target - arr[i];
-      if (map.find(k) != map.end()) {
-        // If it does, return the indices
-        return {map[k], i};
-      }
-      map[arr[i]] = i;
-    }
+    // for (int i = 0; i < arr.size(); i++) {
+    //   int k = target - arr[i];
+    //   if (map.find(k) != map.end()) {
+    //     // If it does, return the indices
+    //     return {map[k], i};
+    //   }
+    //   map[arr[i]] = i;
+    // }
     return {};
   }
 }
